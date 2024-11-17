f6f1d9b96dfb5e4c0035731ec9d26d4833327258
merzann <annisch78@gmail.com>
2024-11-17 19:19:30 +0000

    fix: update displayStatus function to fix key expiry date not displayed in console after removing key from script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
-import { API_KEY } from "../../config";
+import { API_KEY } from "../../config.js";
 
 const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
+// eventListeners for checking the key and post the form
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+// async function for GET request
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
     const response = await fetch(queryString);
@@ -13,14 +18,17 @@ async function getStatus(e) {
     }
 }
 
+// function to display key-info in both, console and modal
 function displayStatus(data) {
-
+    
+    console.log(`Expiry Date: ${data.expiry}`);
+    
     let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
-    resultsModal.show();
 
+    resultsModal.show();
 }
