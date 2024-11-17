b7194ade912de3a534c68ec6741b28b7fe5e54d3
merzann <annisch78@gmail.com>
2024-11-17 19:26:35 +0000

    feat: wire up Run Checks button and add async function for POST request to API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,24 @@ import { API_KEY } from "../../config.js";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
-// eventListeners for checking the key and post the form
+// eventListeners for check key and post the form
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+// async function for POST request to API
+async function postForm(e) {
+
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+}
 
 // async function for GET request
 async function getStatus(e) {
@@ -18,7 +34,7 @@ async function getStatus(e) {
     }
 }
 
-// function to display key-info in both, console and modal
+// function to display key expry date in both, console and modal
 function displayStatus(data) {
     
     console.log(`Expiry Date: ${data.expiry}`);
