6c61a3e61d5747603016789af6d96d890b1a211c
merzann <annisch78@gmail.com>
2024-11-17 20:00:27 +0000

    fix: add processOption function to fix the issue of API receiving options selection in the wrong format

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,27 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+// function to process option selection in correct format expected as input by API
+function processOptions(form) {
+    let optArray = [];
+
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
+        }
+    }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
 // async function for POST request to API
 async function postForm(e) {
 
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
         method: "POST",
